8cdbc68e750999388f72b9b7ad23bc8b655f2ee9
Israel Nebot <israel.nebot@gmail.com>
2018-10-27 01:07:49 +0200

    Start working on UI

---
+++ Assets/Scripts/Isra/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variable references
+    public static GameManager instance;
+    public UIController UIController;
+    #endregion
+
+    public int levelNumber;
+    public float numberOfTotalEnemies;
+    public float numberOfEnemiesSaved;
+    public int doorOpenPercentage;
+
+    public float levelSeconds;
+    private bool timerActivated = false;
+
+    void Awake() { instance = this; }
+
+    public void SetupLevel(int levelNumber, float levelSeconds, int numberOfTotalEnemies)
+    {
+        this.levelNumber = levelNumber;
+        this.levelSeconds = levelSeconds;
+        this.numberOfTotalEnemies = numberOfTotalEnemies;
+
+        UIController.InitializeTimer(levelSeconds);
+
+        Invoke("StartTimer", Time.deltaTime);
+    }
+
+    public void StartTimer() { timerActivated = true; }
+    public void GetNumberOfEnemies() { numberOfTotalEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length; }
+    public void OnEnemySaved()
+    {
+        if(numberOfEnemiesSaved < numberOfTotalEnemies && levelSeconds > 0)
+        {
+            numberOfEnemiesSaved++;
+            doorOpenPercentage = (int) (numberOfEnemiesSaved / numberOfTotalEnemies * 100);
+            if (doorOpenPercentage == 100) OnLevelCompleted();
+        }
+    }
+
+    void OnLevelCompleted()
+    {
+        timerActivated = false;
+        Debug.Log("Level completed!");
+    }
+
+
+    void OnGameOver()
+    {
+        levelSeconds = 0;
+        UIController.ShowInformationText("Sunomáh!", Mathf.Infinity);
+    }
+
+    void Start()
+    {
+        SetupLevel(1, 10, 3);
+    }
+
+    void DecrementTimer()
+    {
+        if (levelSeconds > 0) levelSeconds -= Time.deltaTime;
+        else
+        {
+            OnGameOver();
+        }
+
+        UIController.UpdateTimerGraphic(levelSeconds);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { OnEnemySaved(); }
+        if (timerActivated) { DecrementTimer(); }
+    }
+
+}
---
+++ Assets/Scripts/Isra/UIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public Slider timerSlider;
+    public Text informationText;
+    bool showingInformationText;
+
+    public static UIController instance;
+    void Awake() { instance = this; }
+
+    float levelSeconds;
+   
+
+    public void InitializeTimer(int levelSeconds) { timerSlider.value = timerSlider.maxValue = this.levelSeconds = levelSeconds; }
+    public void InitializeTimer(float levelSeconds) { timerSlider.value = timerSlider.maxValue = this.levelSeconds = levelSeconds; }
+
+    public void UpdateTimerGraphic(float currentSeconds)
+    {
+        timerSlider.value = currentSeconds;
+        timerSlider.targetGraphic.color = Color.Lerp( Color.red, Color.green, currentSeconds / levelSeconds);
+    }
+
+
+    void HideInformationText()
+    {
+        informationText.GetComponent<Animation>().Play("HideInformationText");
+        showingInformationText = false;
+    }
+
+    public void ShowInformationText(string textToShow, float howLongFor)
+    {
+        if (!showingInformationText)
+        {
+            showingInformationText = true;
+            informationText.text = textToShow;
+            informationText.GetComponent<Animation>().Play("ShowInformationText");
+            Invoke("HideInformationText", howLongFor);
+        } 
+    }
+
+
+}
