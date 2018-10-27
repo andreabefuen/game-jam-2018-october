72cbf1b3f451d9842fffa7e40a44ad0c5119321a
axel8890 <axel8890@gmail.com>
2018-10-27 17:02:01 +0200

    SavezoneScript

---
+++ Assets/Scripts/SaveZoneScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveZoneScript : MonoBehaviour
+{
+    public GameObject GameManagerObject;
+    GameManager GMScript;
+
+    List<GameObject> EnemiesSaved = new List<GameObject>();
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GMScript = GameManagerObject.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    public void EnemieEnterSave(GameObject Enemy) {
+        EnemiesSaved.Add(Enemy);
+        GMScript.OnEnemySaved();
+    }
+    public void EnemieEscape()
+    {
+        GMScript.numberOfEnemiesSaved -= EnemiesSaved.Count;
+        foreach (GameObject Enemy in EnemiesSaved)
+        {
+          //  Enemy.GetComponent<MinionsMovements>().RunAway();
+        }
+        EnemiesSaved.Clear();
+    }
+}
