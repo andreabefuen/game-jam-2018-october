fbdfbcbbe8cf639670c123ca39e9a4df6ac77faf
Israel Nebot <israel.nebot@gmail.com>
2018-10-28 15:24:01 +0100

    asdfgh

---
+++ Assets/ExitManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitManager : MonoBehaviour
+{
+    public GameObject exitON;
+    public GameObject exitOFF;
+
+    public static ExitManager instance;
+    void Awake() { instance = this; }
+
+    void Start()
+    {
+        exitON.SetActive(false);
+        exitOFF.SetActive(true);
+    }
+
+    public void ShowExit()
+    {
+        exitOFF.SetActive(false);
+        exitON.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Isra/Level.cs
+++ Assets/Scripts/Isra/Level.cs
@@ -5,13 +5,11 @@ public class Level : MonoBehaviour
     public int numberOfEnemies;
     public int levelSeconds;
 
+
     void Start()
     {
+        GameManager.instance.numberOfTotalEnemies = numberOfEnemies;
         GameManager.instance.numberOfTotalEnemies = numberOfEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
     }
 
-    void OnDestroy()
-    {
-        numberOfEnemies = 0;
-    }
 }
